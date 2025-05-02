1bdd99788d0fc5d0839af7243112a653a829cbf6
Stanislav Kornienko <stkorn@mail.ru>
2025-05-02 13:29:16 +0300

    remove asynchronity

--- modules/h_cover.py
+++ modules/h_cover.py
@@ -8,6 +8,7 @@ import io # For handling byte streams
 import uuid # For generating unique filenames
 import numpy as np # For arrays processing
 import cv2 # For image processing
+import rembg
 from aiogram import Bot, F # For Telegram bot framework
 from aiogram import Router # For creating a router for handling messages
 from aiogram.types import Message, ReactionTypeEmoji, BufferedInputFile # For Telegram message handling
@@ -19,7 +20,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder # For creating inline k
 
 import modules.environment as env # For environment variables and configurations
 import modules.h_start as h_start # For handling start command
-from modules.aiorembg import async_remove # For asynchronous background removal
+#from modules.aiorembg import async_remove # For asynchronous background removal
 
 # Router for handling messages related to processing book covers photos
 cover_router = Router()
@@ -73,7 +74,7 @@ async def cover_photo(message: Message, state: FSMContext, pool: asyncpg.Pool, b
             await message.reply(_("upload_failed"))
             #env.logging.error(f"Error uploading to S3: {e}") # !!!
             print(f"Error uploading to S3: {e}", file=sys.stderr) # !!!
-        
+        '''
         # =========================================================
         # Remove the background from the image
         try:
@@ -85,7 +86,7 @@ async def cover_photo(message: Message, state: FSMContext, pool: asyncpg.Pool, b
         except Exception as e:
             await message.reply(_("remove_background_failed"))
             env.logging.error(f"Error removing background: {e}")
-        
+        '''
         # =========================================================
         # Found book contour
 
